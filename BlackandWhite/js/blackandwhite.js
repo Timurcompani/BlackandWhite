ad033f25126082460723bf6e986d749158a17638
igor gushchyn <igorgushchyn1@gmail.com>
2018-06-18 17:49:43 +0300

    new style

--- BlackandWhite/js/blackandwhite.js
+++ BlackandWhite/js/blackandwhite.js
@@ -1,1 +1,14 @@
-console.log("text");
+console.log("123");
+
+var label = document.getElementById('test');
+console.log(label);
+label.style = "color:blue";
+
+var human = {
+    name:"Igor",
+    age: 49,
+    national:"Ukraine",
+}
+
+
+console.log(human.name+' '+human.age+' '+human.national)
